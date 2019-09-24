8a7a9695a3785de84af232a2aab2e6d0f9b9cf32
Sergei Levkovets <sergeilevkovets@users.noreply.github.com>
2019-09-25 00:32:31 +0300

    homevork#2

---
+++ calculatorofthecost/scripts/myscript.js
@@ -0,0 +1,48 @@
+let form;
+let costCourse;
+let countLessonsOrHours;
+let countLessons;
+let countHours;
+let lessonHours;
+let isCountLessons;
+
+
+document.getElementById('calc').onclick = function () {
+    form = document.forms.my_form;
+    costCourse = form.courseCost.value;
+    countLessonsOrHours = form.lessonsCount.value;
+    lessonHours = form.hoursInLesson.value;
+    isCountLessons = form.elements.count;
+
+
+    if (!calculate()) {
+        return;
+    }
+    let result = calculate();
+    let text = `Стоимость каждого занятия: ${result[0].toFixed(2)} бел.руб<br>
+               стоимость одного часа: ${result[1].toFixed(2)} бел.руб<br>
+               стоимость одной минуты: ${result[2].toFixed(2)} бел.руб<br>
+               стоимость одной секунды: ${result[3].toFixed(4)} бел.руб`;
+    alert(text);
+    document.getElementById("inner_result").innerHTML += text;
+}
+
+function calculate() {
+    if (!costCourse || !countLessonsOrHours || !lessonHours) {
+        alert('Заполните все поля');
+        return;
+    }
+    let costHours;
+    if (isCountLessons[0].checked) {
+        countHours = countLessonsOrHours * lessonHours;
+        costHours = costCourse / countLessonsOrHours / lessonHours;
+        document.getElementById("inner_result").innerHTML = `Количество часов: ${countHours}<br>`;
+        return [costHours * lessonHours, costHours, costHours / 60, costHours / 60 / 60]
+    } else {
+        countLessons = countLessonsOrHours / lessonHours;
+        costHours = costCourse / countLessonsOrHours;
+        document.getElementById("inner_result").innerHTML = `Количество занятий: ${countLessons}<br>`;
+        return [costHours * lessonHours, costHours, costHours / 60, costHours / 60 / 60]
+    }
+}
+
