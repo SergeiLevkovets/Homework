f09db29e6692022559080f13fb4fdd3f50f4aa8c
Sergei Levkovets <sergeilevkovets@users.noreply.github.com>
2019-10-19 15:25:46 +0300

    validation delete final variable

--- homework4-1/electronic-app-form-jsp/src/com/stormnet/task41/util/ValidationImpl.java
+++ homework4-1/electronic-app-form-jsp/src/com/stormnet/task41/util/ValidationImpl.java
@@ -1,22 +1,11 @@
 package com.stormnet.task41.util;
 
 import java.util.HashMap;
-import java.util.LinkedList;
 import java.util.Map;
 
 public class ValidationImpl implements Validation {
 
     private static final String MESSAGE = "<strong style=\"color: red\">Введены неверные данные</strong>";
-    private static final String LAST_NAME = "last_name";
-    private static final String NAME = "name";
-    private static final String PASSWORD = "password";
-    private static final String AGE = "age";
-    private static final String SEX = "sex";
-    private static final String COURSE = "course";
-    private static final String TEACHER = "teacher";
-    private static final String GRADE = "grade";
-    private static final String SOURCES = "sources";
-    private static final String OTHER_TEXT = "other_text";
 
     private Map<String, String> resultMap;
     private boolean isInvalidate = false;
@@ -31,10 +20,10 @@ public class ValidationImpl implements Validation {
     @Override
     public boolean isInvalidate() {
         if (!isContainsSex) {
-            resultMap.put(setErrorsMes(SEX), MESSAGE);
+            resultMap.put(setErrorsMes("sex"), MESSAGE);
         }
         if (!isContainsSources) {
-            resultMap.put(setErrorsMes(SOURCES), MESSAGE);
+            resultMap.put(setErrorsMes("sources"), MESSAGE);
         }
 
         return isInvalidate;
@@ -57,32 +46,32 @@ public class ValidationImpl implements Validation {
         resultMap = new HashMap<>();
         for (String parameterName : parameterMap.keySet()) {
             switch (parameterName) {
-                case LAST_NAME:
-                case NAME:
-                case OTHER_TEXT:
-                case COURSE:
-                case TEACHER:
-                case GRADE:
-                case PASSWORD: {
+                case "last_name":
+                case "name":
+                case "password":
+                case "course":
+                case "teacher":
+                case "grade":
+                case "other_text": {
                     String value = parameterMap.get(parameterName)[0];
                     if (isEmpty(value)) {
                         resultMap.put(setErrorsMes(parameterName), MESSAGE);
                         break;
                     }
                 }
-                case AGE: {
+                case "age": {
                     String value = parameterMap.get(parameterName)[0];
                     if (!isEmpty(value)) {
                         if (!value.matches("[-+]?\\d+")) {
-                            resultMap.put(setErrorsMes(AGE), MESSAGE);
+                            resultMap.put(setErrorsMes(parameterName), MESSAGE);
                         }
                     }
                     break;
                 }
-                case SEX: {
+                case "sex": {
                     isContainsSex = true;
                 }
-                case SOURCES: {
+                case "sources": {
                     isContainsSources = true;
                     break;
                 }
